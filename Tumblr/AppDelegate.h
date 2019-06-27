dfe22d9669370b08b479de9dc7ab7be0de91ac3f
Yuewei Yuan <lucyyyw@fb.com>
2019-06-27 10:26:33 -0700

    Initial Commit

---
+++ Tumblr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tumblr
+//
+//  Created by lucyyyw on 6/27/19.
+//  Copyright © 2019 lucyyyw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tumblr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tumblr
+//
+//  Created by lucyyyw on 6/27/19.
+//  Copyright © 2019 lucyyyw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
